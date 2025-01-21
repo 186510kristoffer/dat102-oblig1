0ec6bc182c3508b966f5d8ab7a98aef1072ee95a
186510kristoffer <krissalbrigtsen@gmail.com>
2025-01-21 14:49:48 +0100

    filmarkiv test oppdatt

--- src/no/hvl/data102/filmarkiv/test/FilmarkivTest.java
+++ src/no/hvl/data102/filmarkiv/test/FilmarkivTest.java
@@ -1,11 +1,11 @@
 package no.hvl.data102.filmarkiv.test;
 
-import static org.junit.jupiter.api.Assertions.*; // For assertions
-import org.junit.jupiter.api.BeforeEach; // For @BeforeEach
-import org.junit.jupiter.api.Test; // For @Test
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import no.hvl.data102.filmarkiv.impl.Filmarkiv;
 import no.hvl.data102.filmarkiv.impl.Film;
+import no.hvl.data102.filmarkiv.impl.Filmarkiv;
 import no.hvl.data102.filmarkiv.impl.Sjanger;
 
 public class FilmarkivTest {
@@ -14,10 +14,11 @@ public class FilmarkivTest {
 
     @BeforeEach
     public void setUp() {
-        // Initialiser et Filmarkiv med plass til 10 filmer
-        arkiv = new Filmarkiv(10);
+        // Opprett et tomt filmarkiv
+        Film[] filmer = new Film[10];
+        arkiv = new Filmarkiv(filmer, 0);
 
-        // Legg til noen testfilmer
+        // Legg til noen filmer
         arkiv.leggTilFilm(new Film(1, "Spielberg", "Jaws", 1975, "Universal", Sjanger.ACTION));
         arkiv.leggTilFilm(new Film(2, "Nolan", "Inception", 2010, "Warner Bros", Sjanger.SCIFI));
     }
@@ -28,7 +29,7 @@ public class FilmarkivTest {
         Film nyFilm = new Film(3, "Lucas", "Star Wars", 1977, "Fox", Sjanger.SCIFI);
         arkiv.leggTilFilm(nyFilm);
 
-        // Sjekk om filmen ble lagt til riktig
+        // Sjekk antall filmer
         assertEquals(3, arkiv.antall(), "Antallet filmer skal være 3 etter å legge til ny film.");
     }
 }
