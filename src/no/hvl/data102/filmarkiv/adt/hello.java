88944b048078724e8e6936c816b990f4ad78ef60
186510kristoffer <krissalbrigtsen@gmail.com>
2025-01-21 13:00:23 +0100

    hello

---
+++ src/no/hvl/data102/filmarkiv/adt/hello.java
@@ -0,0 +1,10 @@
+package no.hvl.data102.filmarkiv.adt;
+
+public class hello {
+	public static void main(String[] args) {
+		for (int i=0; i>999; i++) {
+			System.out.println("hei på deg");
+		}
+	}
+	
+}
--- src/no/hvl/data102/filmarkiv/adt/random.java
+++
@@ -1,5 +0,0 @@
-package no.hvl.data102.filmarkiv.adt;
-
-public class random {
-//hei på deg//
-}
