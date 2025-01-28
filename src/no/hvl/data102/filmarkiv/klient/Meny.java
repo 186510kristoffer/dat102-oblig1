2034f13b4b8ab0687dead1520e07407554994099
EndreNoren <endrenoren@gmail.com>
2025-01-28 13:44:21 +0100

    NY BIG FIX

--- src/no/hvl/data102/filmarkiv/klient/Meny.java
+++ src/no/hvl/data102/filmarkiv/klient/Meny.java
@@ -22,23 +22,23 @@ public class Meny {
 		tekstgr.visFilmArkiv(filmarkiv);
 
 		System.out.println("\nSøker etter filmer med 'Jurassic' i tittelen:");
-		tekstgr.skrivUtFilmerMedDelstrengITittel(filmarkiv, "Jurassic");
+		tekstgr.skrivUtFilmDelstrengITittel(filmarkiv, "Jurassic");
 
 		System.out.println("\nSøker etter filmer av produsenten 'Quentin Tarantino':");
-		tekstgr.skrivUtFilmerAvProdusent(filmarkiv, "Quentin Tarantino");
+		tekstgr.skrivUtFilmProdusent(filmarkiv, "Quentin Tarantino");
 
 		System.out.println("\nAntall filmer i ACTION-sjangeren:");
 		tekstgr.skrivUtStatistikk(filmarkiv);
 	}
 
 	private void leggTilEksempeldata() {
-		filmarkiv.leggTilFilm(new Film(1, "Christopher Nolan", "Inception", 2010, Sjanger.ACTION, "Warner Bros"));
-		filmarkiv.leggTilFilm(new Film(2, "Quentin Tarantino", "Pulp Fiction", 1994, Sjanger.DRAMA, "Miramax"));
+		filmarkiv.leggTilFilm(new Film(1, "Christopher Nolan", "Inception", 2010, "Warner Bros", Sjanger.ACTION));
+		filmarkiv.leggTilFilm(new Film(2, "Quentin Tarantino", "Pulp Fiction", 1994, "Miramax", Sjanger.DRAMA));
 		filmarkiv
-				.leggTilFilm(new Film(3, "Steven Spielberg", "Jurassic Park", 1993, Sjanger.ACTION, "Universal Pictures"));
+				.leggTilFilm(new Film(3, "Steven Spielberg", "Jurassic Park", 1993, "Universal Pictures", Sjanger.ACTION));
 		filmarkiv
-				.leggTilFilm(new Film(4, "James Cameron", "Avatar", 2009, Sjanger.SCIENCE_FICTION, "20th Century Fox"));
+				.leggTilFilm(new Film(4, "James Cameron", "Avatar", 2009, "20th Century Fox", Sjanger.SCIENCE_FICTION));
 		filmarkiv.leggTilFilm(
-				new Film(5, "Peter Jackson", "The Lord of the Rings", 2001, Sjanger.FANTASY, "New Line Cinema"));
+				new Film(5, "Peter Jackson", "The Lord of the Rings", 2001, "New Line Cinema", Sjanger.FANTASY));
 	}
 }
