dd87082dae947dd13e0c796f763148590848d7e2
186510kristoffer <krissalbrigtsen@gmail.com>
2025-01-28 14:35:42 +0100

    tester

---
+++ test/dat102oblig1/JUnitTest.java
@@ -0,0 +1,18 @@
+package dat102oblig1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+package dat102oblig1;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class JUnitTest {
+
+    @Test
+    public void testJUnitIsWorking() {
+        assertEquals(2, 1 + 1, "1 + 1 skal være 2");
+    }
+}
