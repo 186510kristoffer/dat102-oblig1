e0160f7b6435c8ed75c94153c5478ca92705bd10
186469 <elias.daland@gmail.com>
2025-01-30 12:54:31 +0100

    ;)

--- src/no/hvl/data102/filmarkiv/adt/hello.java
+++ src/no/hvl/data102/filmarkiv/adt/hello.java
@@ -3,7 +3,7 @@ package no.hvl.data102.filmarkiv.adt;
 public class hello {
 	
 	public static void main(String[] args) {
-		for (int i=0; i<10000; i++) {
+		for (int i=0; i<10; i++) {
 			System.out.println("Heisann sveisann kjekken");
 		}
 	}
